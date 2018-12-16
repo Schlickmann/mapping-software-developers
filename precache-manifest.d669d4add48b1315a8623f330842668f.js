b8cbfa1227bdb9a895f28436657a8811ef8aacb1
Juliani Schlickmann Damasceno <nany.julychi@gmail.com>
2018-12-15 19:44:18 -0800

    Updates

---
+++ precache-manifest.d669d4add48b1315a8623f330842668f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mapping-software-developers/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mapping-software-developers/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mapping-software-developers/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mapping-software-developers/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mapping-software-developers/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "05d9aa91069016ab736a",
+    "url": "/mapping-software-developers/static/js/runtime~main.05d9aa91.js"
+  },
+  {
+    "revision": "37142c961c40395a3de4",
+    "url": "/mapping-software-developers/static/js/main.37142c96.chunk.js"
+  },
+  {
+    "revision": "a1a93bcab0062f70302f",
+    "url": "/mapping-software-developers/static/js/1.a1a93bca.chunk.js"
+  },
+  {
+    "revision": "a1a93bcab0062f70302f",
+    "url": "/mapping-software-developers/static/css/1.1443e6aa.chunk.css"
+  },
+  {
+    "revision": "65caf3af21d15807879c2baa13c85817",
+    "url": "/mapping-software-developers/index.html"
+  }
+];
